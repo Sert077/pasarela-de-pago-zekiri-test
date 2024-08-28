81e2d748bb8bf2fda5601897b7fb7cb832aa4258
Sergio <servesa07@gmail.com>
2024-08-27 23:33:18 -0400

    home + 4 componentes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,7 @@ const Carousel = () => {
     <section className="bg-gray-100 py-10">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold mb-4 text-black">Integra todos los métodos de pago a tu tienda en línea</h2>
-        <p className="text-lg mb-8 text-black">Acepta pagos en línea con todas las tarjetas de crédito y débito respaldadas por Visa, Mastercard y American Express.</p>
+        <p className="text-lg mb-8 text-black">Acepta pagos en línea con todas las tarjetas de crédito y débito respaldadas por Visa, Mastercard, American Express y otros.</p>
         <div className="flex justify-center space-x-8">
           <img src="/images/visa.png" alt="Visa" className="w-24 h-24 object-contain"/>
           <img src="/images/mscard.png" alt="Mastercard" className="w-24 h-24 object-contain"/>
